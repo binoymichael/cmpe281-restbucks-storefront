7ed479b3b84942ef4363fce66f89d49acfbc392a
pallavikarthick <pallaviatg@gmail.com>
2017-04-24 06:50:53 -0700

    UI integration to delete order

--- orders/show.view.php
+++ orders/show.view.php
@@ -1,8 +1,11 @@
 <?php
+
     $orderId = $orderData['id'];
     $getOrderUrl = '/' . $store . '/order/' . $orderId;
     $deleteOrderUrl = '/' . $store . '/order/' . $orderId;
     $payOrderUrl = '/' . $store . '/order/' . $orderId . '/pay';
+    $getOrdersUrl = '/' . $store . '/orders';
+    //echo "order ID :".$orderId;
 ?>
 <div class="container-fluid text-center headspace">    
   <div class="row content">
@@ -10,9 +13,14 @@
     <h3><?= $orderData['message'] ?></h3>
       <hr>
       <a href=<?= $getOrderUrl ?>  class="btn btn-primary">Order Status<a> 
-      <a href=<?= $deleteOrderUrl ?> class="btn btn-danger">Cancel Order<a> 
+      <form action=<?=$deleteOrderUrl?>  method="post">
+          <button type="submit" class="btn btn-danger">Cancel Order</button>
+      </form>
       <form action=<?=$payOrderUrl?> class="form-inline" method="post">
           <button type="submit" class="btn btn-success">Pay</button>
+      </form>
+      <form action=<?=$getOrdersUrl?> class="form-inline" method="get">
+          <button type="submit" class="btn btn-success">Get Orders</button>
       </form>
     </div>
   </div>
