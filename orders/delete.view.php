f7729209b1d6a79b3441454181f5ec9b7449e15d
Binoy Prakash <prakash.binoy@gmail.com>
2017-05-11 17:45:51 -0700

    Updated delete page

--- orders/delete.view.php
+++ orders/delete.view.php
@@ -4,18 +4,7 @@
 <div class="container-fluid headspace">    
     <div class="col-sm-6 col-sm-offset-1 text-left"> 
       <div class="page-header">
-        <h3>Order successfully cancelled</h3>
+        <h3 style='color:green;font-family: "Indie Flower", cursive;'> <b>Order successfully cancelled</b></h3>
       </div>
     </div>
-  <div class="row content">
-    <div class="col-sm-5 col-sm-offset-1 text-left"> 
-      <dl>
-        <dt>ID : </dt>
-        <dd><?= $orderId ?></dd>
-        <dt>Store : </dt>
-        <dd><?= ucwords($store) ?></dd>
-      </dl>
-    </div>
-    
-  </div>
 </div>
